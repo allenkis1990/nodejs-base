ac6565daa86a13b89cca7a357b20610fed877dc8
Allen <allenkis1990@163.com>
2019-09-17 18:11:17 +0800

    nodebasesubmit

--- process/process1.js
+++ process/process1.js
@@ -14,5 +14,8 @@ console.log(process.arch);//当前cpu多少位
 console.log(process.cwd());//返回当前进程的工作目录 D:\github-project\nodejs-base\process
 
 
-process.stdout.write('这是一行数据\n这是第二行数据');//console.log
-process.stderr.write('输出一行标准错误流，效果跟stdout没差');//console.error
+// process.stdout.write('这是一行数据\n这是第二行数据');//console.log
+// process.stderr.write('输出一行标准错误流，效果跟stdout没差');//console.error
+
+process.stdin.resume()//卡住
+process.exit();//退出
