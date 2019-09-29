ea46037515731bfd85072a0c2cfa898c58f2176a
Allen <allenkis1990@163.com>
2019-09-29 16:59:44 +0800

    子进程

--- cluster/cluster2.js
+++ cluster/cluster2.js
@@ -11,7 +11,9 @@ if(cluster.isMaster){
     var sliceEnd = Math.ceil(strArr.length/cpusNum)
     // console.log(sliceEnd);
     for(let i=0;i<cpusNum;i++){
+        //创建子进程
         var childProcess = cluster.fork()
+        //给相应的子进程发送消息
         childProcess.send({
             idx:i,
             strArr:strArr.slice(i*sliceEnd,(i+1)*sliceEnd)
@@ -25,13 +27,13 @@ if(cluster.isMaster){
     }
 }else{
     // console.log('lwhIndex'+process.lwhIndex+'进程已经运行');
+    // console.log(cluster.worker.id)// 1 2 3 4
     process.on('exit',function(){
         console.log(process.pid+'进程已经退出');
     })
 
+    //各个子进程分别接收到主进程发来的消息
     process.on('message',function(msg){
-        // console.log(msg.idx);
-        // console.log(msg.strArr);
         msg.strArr.forEach(function(item){
             console.log(`我是进程${msg.idx},我正在处理字符串${item}`)
         })
