269c0914bca61fa00f7affb87c69480d28614e24
Allen <allenkis1990@163.com>
2019-09-29 21:08:54 +0800

    子进程

---
+++ debugger/debugger.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Allen Liu on 2019/9/29.
+ */
+//使用webstrom断点，run文件使用debug模式 f8下一个断点  f6一行一行
+
+var express = require('express')
+var app = express()
+var path = require('path')
+console.log(123);
+app.get('/',function(req,res){
+    res.sendFile(path.resolve('./debugger1.html'))
+})
+var test = require('./test.js')
+app.get('/haha',function(req,res){
+    res.send('ok')
+    console.log(123);
+    console.log(123);
+    console.log(123);
+    test.fn()
+    console.log(app)
+})
+
+
+app.listen('9900')
---
+++ debugger/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Allen Liu on 2019/9/29.
+ */
+module.exports = {
+    fn(){
+        var a = 1
+        console.log(a)
+    }
+}
