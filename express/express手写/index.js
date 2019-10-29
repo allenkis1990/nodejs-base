0a9623b03dbd630952eeccc0df431e28e2861162
Allen <allenkis1990@163.com>
2019-10-29 11:18:32 +0800

    express

--- express/express手写/index.js
+++ express/express手写/index.js
@@ -5,6 +5,15 @@
 let express = require('./lwhExpress')
 let app = express()
 
+app.use('/abc',function(req,res,next){
+    req.a =111111
+    next()
+})
+
+app.get('/abc/a1',function(req,res){
+    console.log(req.a);
+    res.end('/abc/a1 ........')
+})
 
 app.get('/',function(req,res){
     // console.log(req);
@@ -16,13 +25,14 @@ app.get('/a',function(req,res){
     res.end('456')
 })
 app.get('/favicon.ico',function(req,res){
+    res.statusCode = '404'
     res.end('123')
 })
 
 app.get('/user/:name/:age',function(req,res){
+    console.log(req.a);
     // console.log(req.reqParams);
     res.end(JSON.stringify(req.reqParams))
 })
-
 
 app.listen('5566')
--- express/express手写/lwhExpress.js
+++ express/express手写/lwhExpress.js
@@ -9,50 +9,78 @@ var routerFns = []
 module.exports = function(){
     var app = {
         get(path,handler){
+            if(!handler){
+                handler = path
+                path = '/'
+            }
             routerFns.push({
                 path:path,
                 method:'get',
                 handler:handler
             })
         },
+        use(path,handler){
+            if(!handler){
+                handler = path
+                path = '/'
+            }
+            routerFns.push({
+                path:path,
+                method:'middle',
+                handler:handler
+            })
+        },
         listenHandler(req,res){
             var routerIndex = 0
-            if(routerIndex>routerFns.length-1){
-                return
-            }
             function next(){
+                if(routerIndex>routerFns.length-1){
+                    res.setHeader('Content-Type','text/plain;charset=utf-8')
+                    res.end('没有匹配的路由')
+                    return
+                }
                 let router = routerFns[routerIndex++]
                 let requestMethod = req.method.toLowerCase()
                 let urlObj = url.parse(req.url)
-                if(urlObj.pathname===router.path&&requestMethod===router.method){
-                    return router.handler(req,res)
-                }else if(requestMethod===router.method&&router.path.includes(':')){
-                    // /user/:name/:age
-                    var paramsReg = /:([^\/]*)/g
-                    var args = []
-                    var routerRegStr = router.path.replace(paramsReg,function(){
-                        // console.log(arguments);
-                        args.push(arguments[1])//name age
-                        //return要换的字符串
-                        return '([^\/]*)'
-                    })
-                    var routerReg  = new RegExp(routerRegStr)//不能加g加了就匹配不出了只能匹配出匹配到的字符串
-
-                    var match = urlObj.pathname.match(routerReg)
-                    if(match){
-                        var [,...list] = urlObj.pathname.match(routerReg)//lwh 18
-                        // console.log(args,list);
-                        //['name','age'] ['lwh',18] ===>  {name:'lwh',age:18}
-                        var reqParams = args.reduce(function(result,cur,idx){
-                            result[cur] = list[idx]
-                            return result
-                        },{})
-                        req.reqParams = reqParams
+                // console.log(urlObj.pathname,routerIndex,routerFns.length);
+                if(router.method!=='middle'){
+                    if(urlObj.pathname===router.path&&requestMethod===router.method){
                         return router.handler(req,res)
+                    }else if(requestMethod===router.method&&router.path.includes(':')){
+                        // /user/:name/:age
+                        var paramsReg = /:([^\/]*)/g
+                        var args = []
+                        var routerRegStr = router.path.replace(paramsReg,function(){
+                            // console.log(arguments);
+                            args.push(arguments[1])//name age
+                            //return要换的字符串
+                            return '([^\/]*)'
+                        })
+                        var routerReg  = new RegExp(routerRegStr+'$')//不能加g加了就匹配不出了只能匹配出匹配到的字符串
+                        console.log(routerReg);
+                        var match = urlObj.pathname.match(routerReg)
+                        if(match){
+                            var [,...list] = urlObj.pathname.match(routerReg)//lwh 18
+                            // console.log(args,list);
+                            //['name','age'] ['lwh',18] ===>  {name:'lwh',age:18}
+                            var reqParams = args.reduce(function(result,cur,idx){
+                                result[cur] = list[idx]
+                                return result
+                            },{})
+                            req.reqParams = reqParams
+                            return router.handler(req,res)
+                        }
+                    }
+                    next()
+                }else{
+                    //中间件处理 next交给用户
+                    if(urlObj.pathname===router.path || router.path==='/'
+                            // 匹配/abc开头   如/abc/a1
+                        || new RegExp((`^${router.path}/`),'g').test(urlObj.pathname)){
+                        return router.handler(req,res,next)
+                    }else{
+                        next()
                     }
                 }
-
-                next()
             }
             next()
         },
