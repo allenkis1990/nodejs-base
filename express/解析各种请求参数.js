fddc801970022898e5341b9da0f7f5e5f226e0cb
Allen <allenkis1990@163.com>
2019-10-30 15:13:03 +0800

    express

--- express/解析各种请求参数.js
+++ express/解析各种请求参数.js
@@ -1,10 +1,19 @@
 /**
  * Created by Allen Liu on 2019/10/29.
  */
+
+//bodyParser
+//express-session
+//cookie-parser
 let express = require('express')
 let app = express()
+//解析表单带文件的可以用这个
 var formdata = require('formidable');
 let path = require('path')
+
+var cookieParser = require('cookie-parser')
+var expressSession = require('express-session')
+
 function onData(req,cb) {
     var form = new formdata.IncomingForm();
     var obj = {
@@ -22,7 +31,25 @@ function onData(req,cb) {
 }
 
 
+app.use(cookieParser('lwh'))
+app.use(expressSession({
+    // name: 'lwh', // 这里是cookie的name，默认是connect.sid
+    secret: 'lwh520', // 建议使用 128 个字符的随机字符串
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 1000, httpOnly: true }
+}));
+
 app.get('/',function(req,res){
+    console.log(req.session.isFirst,'session');
+    req.session.isFirst = 'yes'
+    console.log(req.signedCookies,'cookie');//假如设置了cookie name:lwh如果前端手动修改了cookie会返回name:false
+    res.cookie('name','lwh',{
+        expires:new Date(new Date().getTime()+10000).toGMTString(),
+        maxAge:1000*10,
+        // httpOnly:true,
+        signed:true
+    })
     res.sendFile(path.resolve(__dirname,'express1.html'))
 })
 
@@ -47,4 +74,4 @@ app.post('/fuck',function(req,res){
     res.send('fuck')
 })
 
-app.listen('9090')
+app.listen('6767')
